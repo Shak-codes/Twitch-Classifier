56875f2a05bb8c4361b0ed238f61f1926e054475
Shakir <shakirrahman81@gmail.com>
2025-05-29 12:53:34 -0400

    Reorganizes data

--- rdc/data/message_to_pairwise.py
+++ rdc/data/message_to_pairwise.py
@@ -4,41 +4,43 @@ import csv
 import os
 
 
-def write_pairwise(files, combinations, chats):
-    for input, output in files:
-        df = pd.read_csv(input)
-        for chat in chats:
-            pairwise = {pair: 0 for pair in combinations}
-            chars = 0
-            string = ""
-            for message in df[df['channel'] == chat].message:
-                message = str(message).lower()
-                prev = None
-                for char in message:
-                    string += char
-                    if char == " " and chars >= 1500:
-                        string.replace("  ", " ")
-                        string += "."
-                        pairwise['message'] = string
-                        pairwise['result'] = chat
-                        exists = os.path.exists(output)
-                        with open(output, mode='a', newline='') as outfile:
-                            writer = csv.DictWriter(
-                                outfile, fieldnames=pairwise.keys())
-                            if not exists:
-                                writer.writeheader()
-                            writer.writerow(pairwise)
-                        chars = 0
-                        pairwise = {pair: 0 for pair in combinations}
-                        string = ""
-                    if not char.isalnum():
-                        prev = None
-                        continue
-                    if prev:
-                        pairwise[f"{prev}{char}"] += 1
-                    chars += 1
-                    prev = char
-                string += ". "
+def write_pairwise(infile, combinations, chats, concat_length, shuffle):
+    output = f"pairwise/{'filtered' if 'filtered' in infile else 'regular'}/pairwise{concat_length}{'_shuffled' if shuffle else ''}"
+    df = pd.read_csv(infile)
+    df = df.sample(frac=1, random_state=1995).reset_index(drop=True)
+
+    for chat in chats:
+        pairwise = {pair: 0 for pair in combinations}
+        chars = 0
+        string = ""
+        for message in df[df['channel'] == chat].message:
+            message = str(message).lower()
+            prev = None
+            for char in message:
+                string += char
+                if char == " " and chars >= concat_length:
+                    string.replace("  ", " ")
+                    string += "."
+                    pairwise['message'] = string
+                    pairwise['result'] = chat
+                    exists = os.path.exists(output)
+                    with open(output, mode='a', newline='') as outfile:
+                        writer = csv.DictWriter(
+                            outfile, fieldnames=pairwise.keys())
+                        if not exists:
+                            writer.writeheader()
+                        writer.writerow(pairwise)
+                    chars = 0
+                    pairwise = {pair: 0 for pair in combinations}
+                    string = ""
+                if not char.isalnum():
+                    prev = None
+                    continue
+                if prev:
+                    pairwise[f"{prev}{char}"] += 1
+                chars += 1
+                prev = char
+            string += ". "
 
 
 def get_extremes(files, combinations, chats):
@@ -76,8 +78,9 @@ def get_extremes(files, combinations, chats):
 
 
 def main():
-    files = [("messages.csv", "pairwise.csv"),
-             ("messages_filtered.csv", "pairwise_filtered.csv")]
+    infiles = ["messages.csv", "messages_filtered.csv"]
+    concat_lengths = [500, 1000, 1500]
+    shuffled = [True, False]
 
     chats = ['Mark', 'Dylan', 'Desmond', 'Leland', 'Ben']
 
@@ -85,8 +88,11 @@ def main():
     combinations = [''.join(pair)
                     for pair in itertools.product(letters, repeat=2)]
 
-    write_pairwise(files, combinations, chats)
-    get_extremes(files, combinations, chats)
+    for file in infiles:
+        for length in concat_lengths:
+            for shuffle in shuffled:
+                write_pairwise(file, combinations, chats, length, shuffle)
+    # get_extremes(files, combinations, chats)
 
 
 if __name__ == "__main__":
